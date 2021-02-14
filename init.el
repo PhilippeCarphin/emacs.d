2a7c89ed50313b0369426b4725442ee5bfac91fc
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-02-14 16:01:11 -0500

    Bullets work, I don't know why they didn't work before

--- init.el
+++ init.el
@@ -16,16 +16,3 @@
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit ob-go org-bullets htmlize ox-reveal ox-twbs ox-rst which-key helm evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
