7d654e7254dbf79a7e402153bc322899616be58b
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-02-23 07:58:10 -0500

    Make tab behave correctly in terminal

--- config.el
+++ config.el
@@ -2,22 +2,62 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-(unless (package-installed-p 'use-package) (package-refresh-contents) (package-install 'use-package))
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
 (eval-when-compile (require 'use-package))
+
 (setq scroll-step 1)
 (setq-default scroll-margin 10)
+
 (setq-default auto-fill-function 'do-auto-fill)
 (setq-default fill-column 80)
+
 (load-theme 'misterioso)
+
 (custom-set-faces '(default ((t (:height 200)))))
-(use-package undo-tree :ensure t :config (global-undo-tree-mode))
-(use-package helm :ensure t :preface (require 'helm-config) :config (helm-mode) :bind (("M-x" . helm-M-x) ("C-x C-f" . helm-find-files) ("C-x f" . helm-recentf) ("C-c g" . helm-grep-do-git-grep) ("C-x b" . helm-buffers-list)))
-(use-package which-key :ensure t :delight :init (setq which-key-separator " ") (setq which-key-prefix-prefix "+") (setq which-key-idle-delay 0.01) :config (which-key-mode))
-(use-package company :ensure t :config (global-company-mode) (setq company-idle-delay 0))
-(use-package org-bullets :ensure t :hook (org-mode . org-bullets-mode))
+
+(use-package undo-tree
+  :ensure t
+  :config (global-undo-tree-mode))
+
+(use-package helm :ensure t
+  :preface (require 'helm-config)
+  :config (helm-mode)
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("C-x f" . helm-recentf)
+	 ("C-c g" . helm-grep-do-git-grep)
+	 ("C-x b" . helm-buffers-list)))
+
+(use-package which-key
+  :ensure t
+  :delight
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  (setq which-key-idle-delay 0.01)
+  :config
+  (which-key-mode))
+
+(use-package company
+  :ensure t
+  :config (global-company-mode)
+          (setq company-idle-delay 0))
+
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . org-bullets-mode))
+
 (use-package ox-rst :ensure t)
 (use-package ox-twbs :ensure t)
-(use-package ox-reveal :ensure t :config (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
+(use-package ox-reveal :ensure t
+  :config (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
 (use-package htmlize :ensure t)
-(org-babel-do-load-languages 'org-babel-load-languages '((shell . t) (python . t)))
+
+(org-babel-do-load-languages 'org-babel-load-languages
+    '((shell . t)
+      (python . t)))
+
 (setq org-confirm-babel-evaluate nil)
