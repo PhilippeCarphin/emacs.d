0bf424af03a17e9ee3bb102280da64a653328284
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-02-23 13:30:03 -0500

    Add init.el

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit evil htmlize ox-reveal ox-twbs ox-rst org-bullets company which-key helm undo-tree use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 200)))))
