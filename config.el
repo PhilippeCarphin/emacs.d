b1ca6a4307a9c78cba9f8900f9ad855054ffe047
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-02-24 07:20:44 -0500

    Added gnu to package repo

--- config.el
+++ config.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org"   . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu"   . "http://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
