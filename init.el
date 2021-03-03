4b0e262147a60f02b472d928ff2486dc99776c88
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-03-02 19:34:09 -0500

    Empty init.el

--- init.el
+++ init.el
@@ -1,21 +1,2 @@
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-show-context-detail
-   '((agenda . canonical)
-     (bookmark-jump . canonical)
-     (isearch . canonical)
-     (default . canonical)))
- '(package-selected-packages
-   '(htmlize ox-reveal ox-twbs ox-rst ox-gfm org-bullets evil company which-key helm undo-tree use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 200))))
- '(cursor ((t (:background "SlateGray3")))))
