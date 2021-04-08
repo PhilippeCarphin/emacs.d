e2a6aab44db0355f42e50eb1dc89667f63004d88
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-07 20:33:57 -0400

    Continue instructions on setup

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "$0 : Starting 'emacs --daemon=install'"
+emacs --daemon=install
+echo "$0 : Killing install daemon \"emacsclient -s install -e '(kill-emacs)'\""
+emacsclient -s install -e '(kill-emacs)'
+
+echo "$0 : Linking ec command:"
+
+if [ -e ~/.local/bin/ec ] ; then
+    echo "$0 : ~/.local/bin/ec already exists, not doing anything"
+else
+    ln -v -s $PWD/ec.sh ~/.local/bin/ec
+fi
