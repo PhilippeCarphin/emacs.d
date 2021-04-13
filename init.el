9f200aaec724b993964d622467a1fa20067d1a2f
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-12 21:01:39 -0400

    Put empty (custom-set* ) expressions before org-babel-load-file ...
    so that when marking a theme as safe, it remembers it before the loading of the
    config file.

--- init.el
+++ init.el
@@ -1,2 +1,11 @@
+;;; Custom stuff from Emacs this is there to make Emacs put stuff
+;;; before loading config.org
+(custom-set-variables)
+(custom-set-faces)
+
+
+
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+
