11642602cd84b1ad5e23d7790ad65d945d56454a
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-15 22:49:29 -0400

    Add kill by pid to ec

--- ec.sh
+++ ec.sh
@@ -3,37 +3,36 @@
 function main(){
     # Special actions
     case "$1" in
-	-k)
-	    emacsclient -c -e '(save-buffers-kill-emacs)'
-	    exit 0
-	    ;;
-	-K)
-	    kill_emacs_by_pid
-	    exit 0
-	    ;;
-	-s)
-	    emacs --daemon
-	    exit 0
-	    ;;
-	-r)
-	    emacsclient -e '(kill-emacs)'
-	    emacs --daemon
-	    exit 0
-	    ;;
-	-rs)
-	    emacsclient -e '(kill-emacs)'
-	    emacs --daemon
-	    shift
-	    ;;
+    -k)
+        emacsclient -c -e '(save-buffers-kill-emacs)'
+        exit 0
+        ;;
+    -K)
+        kill_emacs_by_pid
+        exit 0
+        ;;
+    -s)
+        emacs --daemon
+        exit 0
+        ;;
+    -r)
+        emacsclient -e '(kill-emacs)'
+        emacs --daemon
+        exit 0
+        ;;
+    -rs)
+        emacsclient -e '(kill-emacs)'
+        emacs --daemon
+        shift
+        ;;
         -t)
             emacsclient -t $@
             exit $?
             ;;
-	-h) home=$2
-            shift
-	    shift
-            HOME=$home emacs $@
-            exit $?
+    -K)
+        kill_emacs_by_pid
+        exit 0
+        ;;
     esac
 
     if [[ "$SSH_CLIENT" != "" ]] ; then
@@ -81,6 +80,34 @@ function ensure-server-is-running(){
 	read
 	emacs --daemon
     fi
+}
+
+function kill_emacs_by_pid(){
+    if [[ $(uname) == Darwin ]] ; then
+        emacs_process=$(ps -aupcarphin| grep Emacs.app | grep -v grep)
+    else
+        emacs_process=$(ps -aux | grep 'emacs --daemon' | grep -v grep)
+    fi
+    emacs_pid=$(awk '{print $2;}' <<< $emacs_process)
+
+    if [[ -z $emacs_process ]] ; then
+        echo "No emacs process found"
+        return 1
+    fi
+
+    echo "emacs_process: $(awk '{print $2 " " $5;}' <<< $emacs_process)"
+    echo -n "Kill this process? (y/n): "; read answer
+
+    if [[ -z $answer ]] ; then
+        return
+    fi
+
+    if [[ -z $answer ]] || ! ([[ $answer == y ]] || [[ $answer == Y ]]) ; then
+        return
+    fi
+
+    echo kill $emacs_pid
+    kill $emacs_pid
 }
 
 main $@
--- init.el
+++ init.el
@@ -1,7 +1,37 @@
 ;;; Custom stuff from Emacs this is there to make Emacs put stuff
 ;;; before loading config.org
-(custom-set-variables)
-(custom-set-faces)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" default))
+ '(default-frame-alist '((height . 55) (width . 100) (vertical-scroll-bars)))
+ '(indent-tabs-mode nil)
+ '(magit-save-repository-buffers 'dontask)
+ '(org-babel-python-command "python3")
+ '(org-show-context-detail '((occur-tree . ancestors) (default . local)))
+ '(org-startup-folded t)
+ '(safe-local-variable-values
+   '((org-todo-keyword-faces
+      ("GTD-IN" :foreground "#ff8800" :weight normal :underline t :size small)
+      ("GTD-PROJECT" :foreground "#0088ff" :weight bold :underline t)
+      ("GTD-ACTION" :foreground "#0088ff" :weight normal :underline nil)
+      ("GTD-NEXT-ACTION" :foreground "#0088ff" :weight bold :underline nil)
+      ("GTD-WAITING" :foreground "#aaaa00" :weight normal :underline nil)
+      ("GTD-REFERENCE" :foreground "#00ff00" :weight normal :underline nil)
+      ("GTD-SOMEDAY-MAYBE" :foreground "#7c7c74" :weight normal :underline nil)
+      ("GTD-DONE" :foreground "#00ff00" :weight normal :underline nil))))
+ '(vc-follow-symlinks nil)
+ '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 200))))
+ '(cursor ((t (:background "SlateGray3")))))
 
 
 
