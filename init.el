a68fb2d30d70e08d18a4546655ba314d67294882
Philippe Carphin <philippe.carphin2@canada.ca>
2022-11-04 19:40:25 -0400

    Package.el added something... whatever, I'm gonna leave it

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;; Custom stuff from Emacs this is there to make Emacs put stuff
 ;;; before loading config.org
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables)
 (custom-set-faces)
 
