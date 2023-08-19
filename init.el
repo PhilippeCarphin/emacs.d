0f28ad87d0416b88eecb667f3949d0e389b6df7c
Philippe Carphin <philippe.carphin@polymtl.ca>
2023-08-19 15:57:52 -0400

    Random things added to init.el added by Emacs

--- init.el
+++ init.el
@@ -7,8 +7,29 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(custom-set-variables)
-(custom-set-faces)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi))
+ '(custom-safe-themes
+   '("6dc02f2784b4a49dd5a0e0fd9910ffd28bb03cfebb127a64f9c575d8e3651440" "c7f364aeea0458b6368815558cbf1f54bbdcc1dde8a14b5260eb82b76c0ffc7b" default))
+ '(default-frame-alist '((height . 55) (width . 100) (vertical-scroll-bars)))
+ '(indent-tabs-mode nil)
+ '(magit-save-repository-buffers 'dontask)
+ '(org-babel-python-command "python3")
+ '(org-show-context-detail '((occur-tree . ancestors) (default . local)))
+ '(org-startup-folded t)
+ '(vc-follow-symlinks nil)
+ '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 200))))
+ '(cursor ((t (:background "SlateGray3")))))
 
 
 ;;; Load the config
